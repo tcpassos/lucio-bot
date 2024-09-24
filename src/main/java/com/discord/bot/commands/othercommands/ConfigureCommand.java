2adab8e4265f56fbb971772623a78eb9555fac82
tcpassos <tcpassos99@gmail.com>
2024-09-24 16:10:36 -0300

    Update ConfigureCommand.java

--- src/main/java/com/discord/bot/commands/othercommands/ConfigureCommand.java
+++ src/main/java/com/discord/bot/commands/othercommands/ConfigureCommand.java
@@ -1,11 +1,8 @@
 package com.discord.bot.commands.othercommands;
 
 import java.awt.Color;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.discord.bot.commands.ISlashCommand;
-import com.discord.bot.entity.GuildConfig;
 import com.discord.bot.repository.GuildConfigRepository;
 import com.discord.bot.service.MessageService;
 
@@ -15,7 +12,6 @@ import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.components.selections.EntitySelectMenu;
-import net.dv8tion.jda.api.interactions.components.selections.EntitySelectMenu.DefaultValue;
 
 @AllArgsConstructor
 public class ConfigureCommand implements ISlashCommand {
@@ -34,20 +30,10 @@ public class ConfigureCommand implements ISlashCommand {
             return;
         }
 
-        GuildConfig guildConfig = guildConfigRepository.findByGuildId(event.getGuild().getIdLong());
-        if (guildConfig == null) {
-            guildConfig = new GuildConfig(event.getGuild().getIdLong());
-        }
-        List<DefaultValue> defaultValues = new ArrayList<>();
-        if (guildConfig.getGameTextChannelId() != null) {
-            defaultValues.add(DefaultValue.channel(guildConfig.getGameTextChannelId()));
-        }
-
         EntitySelectMenu channelSelectMenu = EntitySelectMenu.create("configure-game-text-channel", EntitySelectMenu.SelectTarget.CHANNEL)
                 .setPlaceholder(messageService.getMessage("ui.gametextchannel.select.placeholder"))
                 .setRequiredRange(1, 1)
                 .setChannelTypes(ChannelType.TEXT)
-                .setDefaultValues(defaultValues)
                 .build();
 
         event.reply(messageService.getMessage("ui.gametextchannel.select.title"))
